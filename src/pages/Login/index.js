21baa5a42f7de54e3266a4776b13fbabc7acedb2
Jorge Valdez <valdez.a.jorge@gmail.com>
2019-06-02 00:32:42 -0500

    added login to login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,21 +1,77 @@
-import React from 'react';
-import {Link} from "react-router-dom";
-import {Box, Form, FormField, Button} from 'grommet';
+import React, { useState } from 'react';
+import {Link, Redirect} from "react-router-dom";
+import {Heading ,Box, Form, FormField, Button} from 'grommet';
+import axios from 'axios';
 
 export default() => {
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [errors, setErrors] = useState(null);
+
+    const onChange = (evt) => {
+        if (errors) {
+            setErrors(null);
+        }
+    };
+
+    const onSubmit = (evt) => {
+        evt.preventDefault();
+
+        const myApi = axios.create({
+            baseURL: 'http://localhost:4000/api',
+            timeout: 10000,
+            withCredentials: true,
+            transformRequest: [(data) => JSON.stringify(data)],
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+            }
+        });
+
+        const { email, password } = evt.value;
+
+        myApi
+            .post('/users/login', {
+                email,
+                password,
+            })
+            .then((response) => {
+                if (response.status !== 200) {
+                    return setErrors(response.data);
+                }
+
+                return setLoggedIn(true);
+            })
+            .catch((err) => {
+                const { data, status } = err.response;
+                if (status !== 200) {
+                    return setErrors(data);
+                }
+            });
+    };
+
+    let errorMessage = null;
+
+    if (loggedIn) {
+        return <Redirect to="/Dashboard" />;
+    }
+
+    else if (errors) {
+        errorMessage = <Heading>{errors.errors.detail}</Heading>
+    }
+
     return (
         <Box pad={{
             horizontal: "xlarge",
             top: "medium"
         }}>
-            <Form>
+            {errors && errorMessage}
+            <Form onSubmit={onSubmit} onChange={onChange}>
                 <FormField name="email" label="Email Address"/>
                 <FormField name="password" label="Password"/>
                 <Box direction="row" pad={{top: "small"}}>
                     <Box margin={{
                         right: "small"
                     }}>
-                        <Link to="/Dashboard"><Button type="submit" primary label="Submit"/></Link>
+                        <Button type="submit" primary label="Submit"/>
                     </Box>
                     <Link to="/Register">
                         <Button type="submit" primary label="Register"></Button>
